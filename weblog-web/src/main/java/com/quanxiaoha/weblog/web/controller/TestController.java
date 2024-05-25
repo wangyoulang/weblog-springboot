d86055445a299de9b3e96b782e81cba5a905597a
wangyoulang <1759923654@qq.com>
2024-05-26 00:27:52 +0800

    JSR 380实现参数校验

--- weblog-web/src/main/java/com/quanxiaoha/weblog/web/controller/TestController.java
+++ weblog-web/src/main/java/com/quanxiaoha/weblog/web/controller/TestController.java
@@ -3,9 +3,15 @@ package com.quanxiaoha.weblog.web.controller;
 import com.quanxiaoha.weblog.web.model.User;
 import com.quanxiaoha.weblog.common.aspect.ApiOperationLog;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.stream.Collectors;
 
 /**
  * @author: 犬小哈
@@ -19,9 +25,13 @@ public class TestController {
 
     @PostMapping("/test")
     @ApiOperationLog(description = "测试接口")
-    public User test(@RequestBody User user) {
-        // 返参
-        return user;
+    public ResponseEntity<String> test(@RequestBody @Valid User user, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            // 获取校验不通过字段的提示信息
+            String errorMsg = bindingResult.getFieldErrors().stream().map(FieldError::getDefaultMessage).collect(Collectors.joining(", "));
+            return ResponseEntity.badRequest().body(errorMsg);
+        }
+        return ResponseEntity.ok("参数没有任何问题");
     }
 
 }
--- weblog-web/src/main/java/com/quanxiaoha/weblog/web/model/User.java
+++ weblog-web/src/main/java/com/quanxiaoha/weblog/web/model/User.java
@@ -2,16 +2,54 @@ package com.quanxiaoha.weblog.web.model;
 
 import lombok.Data;
 
+import javax.validation.constraints.*;
+
 /**
  * @author: 犬小哈
  * @url: www.quanxiaoha.com
  * @date: 2023-08-10 10:35
  * @description: TODO
+ * 以下是 JSR 380 中提供的主要验证注解及其描述：
+ *
+ * @NotNull: 验证对象值不应为 null。
+ * @AssertTrue: 验证布尔值是否为 true。
+ * @AssertFalse: 验证布尔值是否为 false。
+ * @Min(value): 验证数字是否不小于指定的最小值。
+ * @Max(value): 验证数字是否不大于指定的最大值。
+ * @DecimalMin(value): 验证数字值（可以是浮点数）是否不小于指定的最小值。
+ * @DecimalMax(value): 验证数字值（可以是浮点数）是否不大于指定的最大值。
+ * @Positive: 验证数字值是否为正数。
+ * @PositiveOrZero: 验证数字值是否为正数或零。
+ * @Negative: 验证数字值是否为负数。
+ * @NegativeOrZero: 验证数字值是否为负数或零。
+ * @Size(min, max): 验证元素（如字符串、集合或数组）的大小是否在给定的最小值和最大值之间。
+ * @Digits(integer, fraction): 验证数字是否在指定的位数范围内。例如，可以验证一个数字是否有两位整数和三位小数。
+ * @Past: 验证日期或时间是否在当前时间之前。
+ * @PastOrPresent: 验证日期或时间是否在当前时间或之前。
+ * @Future: 验证日期或时间是否在当前时间之后。
+ * @FutureOrPresent: 验证日期或时间是否在当前时间或之后。
+ * @Pattern(regexp): 验证字符串是否与给定的正则表达式匹配。
+ * @NotEmpty: 验证元素（如字符串、集合、Map 或数组）不为 null，并且其大小/长度大于0。
+ * @NotBlank: 验证字符串不为 null，且至少包含一个非空白字符。
+ * @Email: 验证字符串是否符合有效的电子邮件格式。
  **/
 @Data
 public class User {
     // 用户名
+    @NotBlank(message = "用户名不能为空")
     private String username;
     // 性别
+    @NotNull(message = "性别不能为空")
     private Integer sex;
+
+    // 年龄
+    @NotNull(message = "年龄不能为空")
+    @Min(value = 18, message = "年龄必须大于或等于 18")
+    @Max(value = 100, message = "年龄必须小于等于 100")
+    private Integer age;
+
+    // 邮箱
+    @NotBlank(message = "邮箱不能为空")
+    @Email(message = "邮箱格式不正确")
+    private String email;
 }
