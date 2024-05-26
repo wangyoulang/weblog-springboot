2b83dda763ae5f64f1d96549f73a2b1799516369
wangyoulang <1759923654@qq.com>
2024-05-26 19:45:27 +0800

    整合 Knife4j

--- weblog-web/src/main/java/com/quanxiaoha/weblog/web/controller/TestController.java
+++ weblog-web/src/main/java/com/quanxiaoha/weblog/web/controller/TestController.java
@@ -5,6 +5,8 @@ import com.quanxiaoha.weblog.common.exception.BizException;
 import com.quanxiaoha.weblog.common.utils.Response;
 import com.quanxiaoha.weblog.web.model.User;
 import com.quanxiaoha.weblog.common.aspect.ApiOperationLog;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -24,10 +26,12 @@ import java.util.stream.Collectors;
  **/
 @RestController
 @Slf4j
+@Api(tags = "首页模块")
 public class TestController {
 
     @PostMapping("/test")
     @ApiOperationLog(description = "测试接口")
+    @ApiOperation(value = "测试接口OK")
     public ResponseEntity<String> test(@RequestBody @Valid User user, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
             // 获取校验不通过字段的提示信息
@@ -50,12 +54,14 @@ public class TestController {
      */
     @PostMapping("/test1")
     @ApiOperationLog
+    @ApiOperation(value = "测试接口-全局异常处理器：参数校验")
     public Response test1(@RequestBody @Valid User user) {
         return Response.success();
     }
 
     @PostMapping("/test2")
     @ApiOperationLog
+    @ApiOperation(value = "全局异常处理器")
     public Response test2(@RequestBody @Valid User user, BindingResult bindingResult) {
 //        throw new BizException(ResponseCodeEnum.PRODUCT_NOT_FOUND.getErrorCode(), ResponseCodeEnum.PRODUCT_NOT_FOUND.getErrorMessage());
         throw new BizException(ResponseCodeEnum.PRODUCT_NOT_FOUND);
--- weblog-web/src/main/java/com/quanxiaoha/weblog/web/model/User.java
+++ weblog-web/src/main/java/com/quanxiaoha/weblog/web/model/User.java
@@ -1,11 +1,13 @@
 package com.quanxiaoha.weblog.web.model;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import javax.validation.constraints.*;
 
 /**
- * @author: 犬小哈
+ * @author: wangyoulang
  * @url: www.quanxiaoha.com
  * @date: 2023-08-10 10:35
  * @description: TODO
@@ -34,22 +36,27 @@ import javax.validation.constraints.*;
  * @Email: 验证字符串是否符合有效的电子邮件格式。
  **/
 @Data
+@ApiModel(value = "用户实体类")
 public class User {
     // 用户名
     @NotBlank(message = "用户名不能为空")
+    @ApiModelProperty(value = "用户名")
     private String username;
     // 性别
     @NotNull(message = "性别不能为空")
+    @ApiModelProperty(value = "用户性别")
     private Integer sex;
 
     // 年龄
     @NotNull(message = "年龄不能为空")
     @Min(value = 18, message = "年龄必须大于或等于 18")
     @Max(value = 100, message = "年龄必须小于等于 100")
+    @ApiModelProperty(value = "年龄")
     private Integer age;
 
     // 邮箱
     @NotBlank(message = "邮箱不能为空")
     @Email(message = "邮箱格式不正确")
+    @ApiModelProperty(value = "邮箱")
     private String email;
 }
