e4ce5db6b159f66cc6451a8666522ab346d97f65
wangyoulang <1759923654@qq.com>
2024-06-02 23:38:57 +0800

    整合路由器管理，写一个最简单的例子 1、在./pages/frontend中添加一个x.vue 2、在./route目录中添加一个对应的x.js 3、在App.vue中引入路由 4、在main.js中引入App.vue

--- weblog-vue3/src/main.js
+++ weblog-vue3/src/main.js
@@ -1,6 +1,13 @@
-import './assets/main.css'
+// import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+// 导入路由
+import router from './router'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+// 应用路由
+app.use(router)
+app.mount('#app')
+
