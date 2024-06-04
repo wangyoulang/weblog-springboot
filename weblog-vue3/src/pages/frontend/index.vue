56a0466787bb892a3ed26ab4bd7a37099dd71fe4
wangyoulang <1759923654@qq.com>
2024-06-04 21:41:43 +0800

    整合Tailwind CSS

--- weblog-vue3/src/pages/frontend/index.vue
+++ weblog-vue3/src/pages/frontend/index.vue
@@ -1,3 +1,5 @@
 <template>
-    <h1>首页</h1>
+    <div class="bg-green-300 inline">绿色</div>
+    <div class="bg-yellow-300 ml-2 inline">黄色</div>
+    <div class="bg-blue-300 ml-2 inline">蓝色</div>
 </template>
